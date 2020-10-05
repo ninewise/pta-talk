6cbc27ccc6681001a8c2095f9f1d8e47d368bb0b
Francis Begyn <francis@begyn.be>
2020-10-05 21:13:51 +0200

    Update tab-transaction.sh

--- ledger-imports/tab-transaction.sh
+++ ledger-imports/tab-transaction.sh
@@ -5,8 +5,8 @@ if [[ -f demo.csv ]]; then
 fi
 
 rawjson=$(curl -H "Accept: application/json" \
-	-H "Authorization: Token token=ELXlqz9M0JXT8o8PClh8Bg==" \
-	https://tab.zeus.gent/users/thecy/transactions)
+	-H "Authorization: Token token=${TAB_TOKEN}" \
+	https://tab.zeus.gent/users/${TAB_USER}/transactions)
 
 correctAmount=$(echo $rawjson | jq '.[0:100] | .[] | .amount = .amount/100')
 correctAmount=$(echo $correctAmount | jq '[inputs]')
